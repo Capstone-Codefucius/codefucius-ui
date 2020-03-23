8c674161ed67b9883d2c54a369916e1a65dece88
Sam <brunersamuel123@gmail.com>
2020-03-23 13:52:42 -0400

    created the theme and animation for table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, animate, transition, } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // animations: [trigger('triggerName'), [state('stateName', style()) transition('stateChangeExpression', [Animation Steps])] ]
 })
 
 export class AppComponent {

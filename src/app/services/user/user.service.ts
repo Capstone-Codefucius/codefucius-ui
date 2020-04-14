68a8448e328a1750ad6ce01429a2cdbb0430c3a2
Elizabeth Hutchison <elizabeth68.h@gmail.com>
2020-04-14 12:54:26 -0400

    admin table and styling

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -7,8 +7,8 @@ import { User } from '../../services/user/user';
   providedIn: 'root'
 })
 export class UserService {
-  //public API = 'https://localhost:5000/api';  //make sure uncommented for GitHub
-  public API = 'https://localhost:44363/api'; //for Kristi's env local only
+  public API = 'http://localhost:5000/api';  //make sure uncommented for GitHub
+  //public API = 'https://localhost:44363/api'; //for Kristi's env local only
 
   public CODEFUCIUS_API = `${this.API}/user`;
   constructor(private http: HttpClient) { }

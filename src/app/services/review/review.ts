33088ebbde8048c096b40e974599db2ea308b2c9
Kristi <kltakach@gmail.com>
2020-04-13 22:38:16 -0400

    updated review service to use the api. cards are filtering. html & css is rough.

---
+++ src/app/services/review/review.ts
@@ -0,0 +1,9 @@
+export class Review {
+    public ID: string;
+    public AuthorID: string;
+    public Title: string;
+    public status: string;    
+    public Description: string;
+    public Created: string;
+    public Deadline: string;
+}

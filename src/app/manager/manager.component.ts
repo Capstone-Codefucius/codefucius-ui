584bc4637a0e566802c7667f67448afa32750404
Kristi <kltakach@gmail.com>
2020-04-21 17:50:04 -0400

    table is pulling in values from API.

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -3,23 +3,25 @@ import { MatTableDataSource } from '@angular/material/table';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { UserService } from '../services/user/user.service';
 import { User } from '../services/user/user';
-import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
+// import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
 
 
-/* export interface ManagerTable {
+export interface ManagerTable {
   name: string;
   role: string;
   reviewHours: number;
   value: number;
   completedReviews: number;
-} */
+}
 
-/*  const ELEMENT_DATA: ManagerTable[] = [
-  {name: 'Emily McDonald', role: 'Reviewer', reviewHours: 10, value: 10, completedReviews: 6},
-  {name: 'Bill Smile', role: 'Reviewer', reviewHours: 15, value: 15, completedReviews: 10},
-  {name: 'Will Desk', role: 'Reviewer', reviewHours: 6, value: 6, completedReviews: 8},
-  {name: 'Smith Will', role: 'Reviewer', reviewHours: 1, value: 1, completedReviews: 15}
-];  */
+const ELEMENT_DATA: ManagerTable[] = [
+  { name: 'Greg Mann', role: 'Reviewee', reviewHours: 3.5,value: 3.5, completedReviews: 6},
+  { name: 'Danny Osborne', role: 'Manager', reviewHours: 2, value: 2, completedReviews: 6},
+  { name: 'Mary Mercer', role: 'Reviewee', reviewHours: 5.5, value: 5.5, completedReviews: 6},
+  { name: 'Terrence Schubert', role: 'Reviewer', reviewHours: 0, value: 0, completedReviews: 6},
+  { name: 'Marie Leon', role: 'Admin', reviewHours: 0, value: 0, completedReviews: 6}
+
+];
 
 @Component({
   selector: 'app-manager',
@@ -28,31 +30,20 @@ import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector'
 })
 export class ManagerComponent implements OnInit {
   //displayedColumns: string[] = ['name', 'role', 'reviewHours', 'completedReviews'];
-  //dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-  displayedColumns: string[] = ['name', 'role', 'reviewHours'];
-  users: Array<User>;
-  dataSource: UserService;
+  displayedColumns: string[] = ['name', 'role', 'reviewHours', 'value', 'completed'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
   }
+  ELEMENT_DATA: any[];
 
-  /*  assignCopy() {
-     this.users = Object.assign([], this.users);
-   }
-   filterItem(value) {
-     if (!value) {
-       this.assignCopy();
-     } // when nothing has typed
-     this.users = Object.assign([], this.users).filter(
-       item => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
-     )
-     this.assignCopy();//when you fetch collection from server.
- 
-   }; */
+  //maybe filter list here, then the box just calls it
+
+  users: Array<User>;
+  // dataSource: UserService;
 
 
 
@@ -70,10 +61,10 @@ export class ManagerComponent implements OnInit {
   reviewDateFrom: Date;
 
   constructor(private UserService: UserService) {
-    // Object.assign(this, { ELEMENT_DATA })
-    Object.assign(this, { User })
+    Object.assign(this, { ELEMENT_DATA })
+    // Object.assign(this, { User })
   }
-
+ 
   //call to API to get names
   ngOnInit() {
     this.UserService.getAll().subscribe(res => {
